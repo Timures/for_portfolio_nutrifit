e19bd3aaa45edf24ff74471a013b7fe9b73fe6ad
Timures <bisemsk@gmail.com>
2019-10-15 23:42:11 +0600

    add vue - 8.08

---
+++ src/components/Example.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="main">
+        <div class="container">
+            <h1>Webpack</h1>
+            <img src="/assets/img/dog.jpg" alt="dog">
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: green;
+    }
+</style>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './js/common'
 import './css/main.css'
 import './scss/main.scss'
 window.Vue = require('vue')
+
+Vue.component('example-component', require('./components/Example.vue').default)

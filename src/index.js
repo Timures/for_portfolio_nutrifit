323ec04d0fc58f50ba5a19b4b667ef062a99e80d
Timures <bisemsk@gmail.com>
2019-10-28 23:48:04 +0600

    add hash to js and css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import store from './store'
 Vue.component('example-component', require('./components/Example.vue').default)
 
 const app = new Vue({
+    data() {
+        return {
+            component: false,
+        }
+    },
     store,
     el: '#app'
 })
